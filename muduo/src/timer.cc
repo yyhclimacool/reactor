0d5262a75bdbbcf19b6d3f897af634aec5543e16
yyhclimacool <yyhclimacool@gmail.com>
2021-12-21 14:55:21 +0000

    add class Timer & TimerId

--- muduo/src/timer.cc
+++ muduo/src/timer.cc
@@ -1,15 +1,15 @@
-/* #include "muduo/src/timer.h"
+#include "timer.h"
 
 using namespace muduo;
 
 std::atomic<int64_t> Timer::_s_num_created;
 
-void Timer::Restart(Timestamp now) {
-    if (_repeat) {
-        now += _interval;
-        _expiration = now;
-    } else {
-        _expiration = Timestamp::Invalid();
-    }
+void Timer::restart(Timestamp now) {
+  if (_repeat) {
+    now += _interval;
+    _expiration = now;
+  } else {
+    // 非repeat定时器不能restart
+    _expiration = Timestamp::invalid();
+  }
 }
- */
--- muduo/src/timer.h
+++ muduo/src/timer.h
@@ -1,9 +1,9 @@
-/* #pragma once
+#pragma once
 
 #include <atomic>
 #include <functional>
 
-#include "muduo/src/timestamp.h"
+#include "timestamp.h"
 
 namespace muduo {
 
@@ -11,41 +11,45 @@ class Timer;
 class TimerQueue;
 
 class TimerId {
-    friend class TimerQueue;
+  friend class TimerQueue;
+
 public:
-    TimerId() : _timer(nullptr), _sequence(0) {}
-    TimerId(Timer *timer, int64_t seq) : _timer(timer), _sequence(seq) {}
+  TimerId()
+      : _timer(nullptr)
+      , _sequence(0) {}
+  TimerId(Timer *timer, int64_t seq)
+      : _timer(timer)
+      , _sequence(seq) {}
+
 private:
-    Timer *_timer;
-    int64_t _sequence;
+  Timer  *_timer;
+  int64_t _sequence;
 };
 
 class Timer {
 public:
-    Timer(std::function<void()> cb,
-          Timestamp when,
-          double interval)
-        : _callback(std::move(cb)),
-          _expiration(when),
-          _interval(interval),
-          _repeat(interval > 0.0),
-          _sequence(++_s_num_created) { }
+  Timer(std::function<void()> cb, Timestamp when, double interval)
+      : _callback(std::move(cb))
+      , _expiration(when)
+      , _interval(interval)
+      , _repeat(interval > 0.0)
+      , _sequence(++_s_num_created) {}
 
-    void Run() const { _callback(); }
-    Timestamp Expiration() const { return _expiration; }
-    bool Repeat() const { return _repeat; }
-    int64_t Sequence() const { return _sequence; }
-    void Restart(Timestamp now);
-    static int64_t NumCreated() { return _s_num_created; }
+  void           run() const { _callback(); }
+  Timestamp      expiration() const { return _expiration; }
+  bool           repeat() const { return _repeat; }
+  int64_t        sequence() const { return _sequence; }
+  void           restart(Timestamp now);
+  static int64_t num_created() { return _s_num_created; }
+
 private:
-    const std::function<void()> _callback;
-    Timestamp _expiration;
-    const double _interval;
-    const bool _repeat;
-    const int64_t _sequence;
+  const std::function<void()> _callback;
+  Timestamp                   _expiration;
+  const double                _interval;
+  const bool                  _repeat;
+  const int64_t               _sequence;
 
-    static std::atomic<int64_t> _s_num_created;
+  static std::atomic<int64_t> _s_num_created;
 };
 
 } // namespace muduo
- */
